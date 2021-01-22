5a4f021a39b5c254229cf6f802cca381abd6b51b
Jonathan-BB <74646661+Jonathan-BB@users.noreply.github.com>
2021-01-22 15:22:29 +0100

    Started next stage
    started next stage and added enemies but still need to code the random generation of stages.

---
+++ Assigment/Assets/Pause.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+    
+    public bool paused;
+    void Start () { 
+    paused = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetMouseButtonDown(0)) {
+            paused = !paused;
+        }
+        if (paused) {  
+            Time.timeScale = 0;
+
+        }
+        
+        else if (!paused) {
+            Time.timeScale = 1;
+        }
+
+    }
+}
